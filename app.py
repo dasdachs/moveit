43164460c0c609194349131455e7472d51804bb9
dasDachs <jani.sumak@gmail.com>
2017-04-19 23:06:51 +0200

    Changed DB settings

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
 from flask import Flask, jsonify, render_template, request
+import os
+
 from pymongo import ASCENDING, DESCENDING
 from flask_pymongo import PyMongo
 
 
 # Create app
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = "moveit"
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME") or "moveit"
 mongo = PyMongo(app)
 
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/today")
+def today():
+    data = mongo.db.movements.find(
+        projection={"_id": False,"date": True}, 
+        sort=[("date", DESCENDING)]
+    )
+    data = list(data)
+    return render_template("today.html", data=data)
 
 @app.route("/history")
 def history():
